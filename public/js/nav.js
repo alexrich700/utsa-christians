2d154bd270e97325c16d8e8fad8d821b2de5bf2e
Jose Gamboa <jl.gamboachu@gmail.com>
2018-12-14 21:10:20 +0000

    v2.0

--- public/js/nav.js
+++ public/js/nav.js
@@ -44,22 +44,22 @@ if($(window).width() < 767) {
     
     
 // for countdown timer    
-    const second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
+    // const second = 1000,
+    // minute = second * 60,
+    // hour = minute * 60,
+    // day = hour * 24;
 
-    let countDown = new Date('Sept 4, 2018 0:00:00').getTime(),
-    x = setInterval(function() {
+    // let countDown = new Date('Sept 4, 2018 0:00:00').getTime(),
+    // x = setInterval(function() {
 
-      let now = new Date().getTime(),
-        distance = countDown - now;
+    //   let now = new Date().getTime(),
+    //     distance = countDown - now;
 
-        document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-    }, second)
+    //     document.getElementById('days').innerText = Math.floor(distance / (day)),
+    //     document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+    //     document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+    //     document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+    // }, second)
     
     
 // toggle show more btn on index page    
